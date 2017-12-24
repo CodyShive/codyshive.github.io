1a085cf3aae3222b8f0ec2fac5d248efeebdac74
CodyShive <34662215+CodyShive@users.noreply.github.com>
2017-12-23 19:14:51 -0500

    Update highchartsCS.js

--- highchartsCS.js
+++ highchartsCS.js
@@ -4,6 +4,12 @@ function styleHighcharts() {
    * @author Torstein Honsi
    */
 
+  Highcharts.setOptions({
+    lang: {
+      decimalPoint: '.',
+      thousandsSep: ','
+    }
+  });
   Highcharts.theme = {
      colors: ["#2b908f", "#90ee7e", "#f45b5b", "#7798BF", "#aaeeee", "#ff0066", "#eeaaee",
         "#55BF3B", "#DF5353", "#7798BF", "#aaeeee"],
